98105fa6006cdcb865e79fcbbdf8848b5d005d0d
Abdelilah <abdouajjouqa@gmail.com>
2025-01-14 22:18:37 +0100

    create interface class

---
+++ classes/authentication.php
@@ -0,0 +1,18 @@
+<?php 
+
+interface Authentication
+{
+    public function register(PDO $db, $userName, $password, $comfirmPassword);
+    
+    public function login(PDO $db, $password);
+
+    public function logout();
+
+    public function isLoggedIn();
+
+    public function isAdmin();
+
+    public function isTeacher();
+
+    public function usStudent();
+}
