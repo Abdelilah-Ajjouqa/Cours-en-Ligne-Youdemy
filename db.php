ca533ad40a54089fdf5030e382f6607b4a5ff964
Abdelilah <abdouajjouqa@gmail.com>
2025-01-14 15:58:35 +0100

    connect to database and add register's and login's pages

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+    private $host = 'localhost';
+    private $dbname = 'youdemy';
+    private $user = 'root';
+    private $password = '123';
+    private $conn;
+
+    public function __construct(){
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->user, $this->password);
+            echo 'Connected successfully';
+        } catch (PDOException $e){
+            echo 'Connection failed: ' . $e->getMessage();
+            exit();
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+
+$database = new Database();
+$pdo = $database->getConnection();
