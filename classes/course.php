4dbf7e87b8323c73aa55a37db74a949ecc959c21
Abdelilah <abdouajjouqa@gmail.com>
2025-01-20 15:47:48 +0100

    feat: enhance Courses class with properties and addCourse method for course creation

--- classes/course.php
+++ classes/course.php
@@ -1,6 +1,18 @@
 <?php
 
 class Courses {
+    protected $cover;
+    protected $title;
+    protected $description;
+    protected $content;
+
+    public function __construct($cover, $title, $description, $content)
+    {
+        $this->cover = $cover;
+        $this->title = $title;
+        $this->description = $description;
+        $this->content = $content;
+    }
 
     public function getAllCours(PDO $db)
     {
@@ -15,8 +27,17 @@ class Courses {
         }
     }
 
-    public function addCourse() {
-        // code here
+    public function addCourse(PDO $db) {
+        $query = 'INSERT INTO courses (cover, title, description, content) VALUES (:cover, :title, :description, :content)';
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':cover', $cover);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':content', $content);
+        $stmt->execute();
+
+        $courseId = $db->lastInsertId();
+
     }
 
     public function getCourseDetails()
