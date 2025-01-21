03e8a13b5ca2217b45278df49246a5be58332d37
Abdelilah <abdouajjouqa@gmail.com>
2025-01-21 13:47:20 +0100

    refactor: reorganize course class structure and update delete course form path

---
+++ forms/delete-course.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../db.php';
+require_once '../classes/course.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
--- pages/teacher.php
+++ pages/teacher.php
@@ -105,8 +105,8 @@ if (!isset($_SESSION['email'])) {
                                 <a href="#" class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 transition duration-300">Edit</a>
                             </td>
                             <td class="py-3 px-4">
-                                <form action="delete_course.php" method="post" onsubmit="return confirm('Are you sure you want to delete this course?');">
-                                    <input type="hidden" name="id" >
+                                <form action="../forms/delete-course.php" method="post" onsubmit="return confirm('Are you sure you want to delete this course?');">
+                                    <input type="hidden" name="id">
                                     <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300">Delete</button>
                                 </form>
                             </td>
