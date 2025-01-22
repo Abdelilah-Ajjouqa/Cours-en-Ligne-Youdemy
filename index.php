545ddeb3cb1ad002e2014bb087bb1143f02e0d78
Abdelilah <abdouajjouqa@gmail.com>
2025-01-22 17:26:24 +0100

    feat: implement home page with user session handling and navigation

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-require '../db.php';
-require '../classes/user.php';
-require '../classes/courses.php';
+require '../../db.php';
+require '../../classes/user.php';
+require '../../classes/courses.php';
 
 $data = new Database;
 $conn = $data->getConnection();
@@ -72,41 +72,36 @@ if (!isset($_SESSION['email'])) {
         </div>
     </div>
 
-    <!-- admin -->
-    <?php
-    header("location: ./admin/admin.php");
-    ?>
+    <!-- main content -->
+    <section class="bg-gray-50">
+        <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+            <div class="mx-auto max-w-xl text-center">
+                <h1 class="text-3xl font-extrabold sm:text-5xl">
+                    Understand User Flow.
+                    <strong class="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
+                </h1>
 
-    <!-- teacher -->
-    <?php
-    if ($role == 'teacher') {
-        header("location: ./teacher/teacher.php");
-    }
-    ?>
+                <p class="mt-4 sm:text-xl/relaxed">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+                    numquam ea!
+                </p>
 
-    <!-- student -->
-    <?php
-    if ($role == 'student') {
-        header("location: ../index.html");
-    }
-    ?>
+                <div class="mt-8 flex flex-wrap justify-center gap-4">
+                    <a
+                        class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                        href="#">
+                        Get Started
+                    </a>
 
-    <script>
-        function courseForm() {
-            var form = document.getElementById('courseForm');
-            form.classList.remove('hidden');
-        }
-
-        function cancelForm() {
-            var form = document.getElementById('courseForm');
-            form.classList.add('hidden');
-        }
-
-        document.addEventListener('DOMContentLoaded', function() {
-            var form = document.getElementById('courseForm');
-            form.classList.add('hidden');
-        })
-    </script>
+                    <a
+                        class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                        href="#">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </body>
 
