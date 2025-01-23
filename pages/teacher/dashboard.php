00130909f026e95ab4e35282ebab7591f3df8f35
Abdelilah <abdouajjouqa@gmail.com>
2025-01-23 08:55:56 +0100

    fix: update navigation links to point to allCourses.php instead of index.php

--- pages/teacher/dashboard.php
+++ pages/teacher/dashboard.php
@@ -34,12 +34,12 @@ if (!isset($_SESSION['email'])) {
     <!-- navbar -->
     <div class="navbar flex justify-between bg-white shadow-md p-4">
         <div class="flex-1">
-            <a class="text-2xl font-bold text-indigo-600 hover:text-2xl duration-300" href="../index.php"><i
+            <a class="text-2xl font-bold text-indigo-600 hover:text-2xl duration-300" href="../courses/allCourses.php"><i
                     class="text-red-500">You</i>demy</a>
         </div>
         <div class="flex-none">
             <ul class="flex space-x-4">
-                <li><a class="text-gray-700 hover:text-indigo-600" href="../index.php">Home</a></li>
+                <li><a class="text-gray-700 hover:text-indigo-600" href="../courses/allCourses.php">Home</a></li>
                 <li><a class="text-gray-700 hover:text-indigo-600" href="#">Blog</a></li>
                 <li><a class="text-gray-700 hover:text-indigo-600" href="#">Contact</a></li>
                 <li>
