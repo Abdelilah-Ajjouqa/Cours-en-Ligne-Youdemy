f05346277851c1e9a31fb5dd4e69e6c5bc11d3c9
Abdelilah <abdouajjouqa@gmail.com>
2025-01-23 14:03:36 +0100

    feat: update admin dashboard to display all courses and improve navbar structure

--- pages/admin/admin.php
+++ pages/admin/admin.php
@@ -27,7 +27,7 @@ if (!isset($_SESSION['email'])) {
     // var_dump($user);
 
     // get all courses
-    $course = Courses::getAllCourses($conn);
+    $courses = Courses::getAllCourses($conn);
     // var_dump($course);
 
     // get all students
@@ -53,39 +53,39 @@ if (!isset($_SESSION['email'])) {
 <body class="bg-blue-50">
     <!-- navbar -->
     <div class="navbar flex justify-between bg-white shadow-md p-4">
-            <div class="flex-1">
-                <a class="text-2xl font-bold text-indigo-600 hover:text-2xl duration-300" href="../courses/allCourses.php"><i class="text-red-500">You</i>demy</a>
-            </div>
-            <div class="flex-none">
-                <ul class="flex space-x-4">
-                    <li><a class="text-gray-700 hover:text-indigo-600" href="../courses/allCourses.php">Courses</a></li>
-                    <!-- <li><a class="text-gray-700 hover:text-indigo-600" href="#">Blog</a></li> -->
-                    <li><a class="text-gray-700 hover:text-indigo-600" href="#">Contact</a></li>
-                    <li>
-                        <details class="relative">
-                            <summary class="hover:underline text-red-600 cursor-pointer hover:text-indigo-600" style="list-style: none;">
-                                <?php
-                                echo $username;
-                                ?>
-                            </summary>
-                            <ul class="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
-                                <li>
-                                    <a class="block px-4 py-2 text-gray-700 hover:bg-indigo-600 hover:text-white" href="#">
-                                        Dashboard
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="block px-4 py-2 text-gray-700 hover:bg-indigo-600 hover:text-white" href="#">
-                                        <form action="../../forms/logout.php" method="post">
-                                            <input type="submit" value="Logout" class="block w-full text-left">
-                                        </form>
-                                    </a>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
-            </div>
+        <div class="flex-1">
+            <a class="text-2xl font-bold text-indigo-600 hover:text-2xl duration-300" href="../courses/allCourses.php"><i class="text-red-500">You</i>demy</a>
+        </div>
+        <div class="flex-none">
+            <ul class="flex space-x-4">
+                <li><a class="text-gray-700 hover:text-indigo-600" href="../courses/allCourses.php">Courses</a></li>
+                <!-- <li><a class="text-gray-700 hover:text-indigo-600" href="#">Blog</a></li> -->
+                <li><a class="text-gray-700 hover:text-indigo-600" href="#">Contact</a></li>
+                <li>
+                    <details class="relative">
+                        <summary class="hover:underline text-red-600 cursor-pointer hover:text-indigo-600" style="list-style: none;">
+                            <?php
+                            echo $username;
+                            ?>
+                        </summary>
+                        <ul class="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+                            <li>
+                                <a class="block px-4 py-2 text-gray-700 hover:bg-indigo-600 hover:text-white" href="#">
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li>
+                                <a class="block px-4 py-2 text-gray-700 hover:bg-indigo-600 hover:text-white" href="#">
+                                    <form action="../../forms/logout.php" method="post">
+                                        <input type="submit" value="Logout" class="block w-full text-left">
+                                    </form>
+                                </a>
+                            </li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </div>
     </div>
 
     <!-- main content -->
@@ -93,13 +93,13 @@ if (!isset($_SESSION['email'])) {
         <h1 class="text-4xl font-bold text-center text-indigo-600 mb-4">Admin Dashboard</h1>
         <div class="grid grid-cols-3 gap-4">
             <div class="bg-white p-4 shadow-md rounded-md">
-                <h2 class="text-xl">Courses</h2>
+                <h2 class="text-xl">All Courses</h2>
                 <ul>
-                    <li class="text-gray-700">Course 1</li>
-                    <li class="text-gray-700">Course 2</li>
-                    <li class="text-gray-700">Course 3</li>
-                    <li class="text-gray-700">Course 4</li>
-                    <li class="text-gray-700">Course 5</li>
+                    <?php foreach ($courses as $course): ?>
+                        <li class="text-gray-700 py-2 border-b border-gray-200">
+                            <span class="font-semibold"><?php echo htmlspecialchars($course['title']); ?></span>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
             <div class="bg-white p-4 shadow-md rounded-md">
